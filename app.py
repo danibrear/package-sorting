eedccd6634cba5688dadf06022ed6421a368bbcf
Dani Brear <danielle@phoenixtechnologies.io>
2026-02-11 23:23:41 -0500

    sorting

---
+++ app.py
@@ -0,0 +1,29 @@
+BULKY_VOL = 1_000_000
+BULKY_DIM = 150
+
+HEAVY_WEIGHT = 20
+
+def is_bulky(width, height, length):
+    over_dim = max(width, height, length) >= BULKY_DIM
+    over_area = width * height * length >= BULKY_VOL
+    return over_dim or over_area
+
+def is_heavy(mass):
+    return mass >= HEAVY_WEIGHT
+
+def sort(width, height, length, mass):
+    heavy = is_heavy(mass)
+    bulky = is_bulky(width, height, length)
+    return ['STANDARD', 'SPECIAL', 'REJECTED'][bulky + heavy]
+
+assert sort(50, 50, 50, 10) == 'STANDARD'
+assert sort(200, 50, 50, 10) == 'SPECIAL'
+assert sort(50, 200, 50, 10) == 'SPECIAL'
+assert sort(50, 50, 200, 10) == 'SPECIAL'
+assert sort(50, 50, 50, 30) == 'SPECIAL'
+assert sort(200, 50, 50, 30) == 'REJECTED'
+assert sort(50, 200, 50, 30) == 'REJECTED'
+assert sort(50, 50, 200, 30) == 'REJECTED'
+assert sort(100, 100, 100, 10) == 'SPECIAL'
+assert sort(100, 100, 99, 10) == 'STANDARD'
+assert sort(100, 100, 100, 20) == 'REJECTED'
